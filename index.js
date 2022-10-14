8ca47ba777ad444c7aad841bdbdd432bf33b229d
Tyler Childs <email@tychi.me>
2022-10-13 19:45:57 -0700

    migrate from bare node to bare express

---
+++ index.js
@@ -0,0 +1,14 @@
+var bus = require('statebus')()
+const express = require('express')
+const app = express()
+const port = 1989
+
+app.use(bus.libs.http_in)
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
+
+// Setup the statebus!
+bus.honk = 1                // Print handy debugging output
+bus.libs.file_store()       // Persist state onto disk
