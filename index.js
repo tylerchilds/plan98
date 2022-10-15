e60bf0df124f7d77215acf4d4006af6b9d990df2
Tyler Childs <email@tychi.me>
2022-10-14 17:12:04 -0700

    don't like error on first hit plz

--- index.js
+++ index.js
@@ -30,9 +30,42 @@ const supabase = createClient(
 const app = express()
 const port = 1989
 
-
+/*
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+
+app.all('*', checkUser);
+
+function checkUser(req, res, next) {
+  const nonSecurePaths = ['/', '/about', '/contact'];
+  // data is public by default-- not a rule, but an artistic liberty
+  if (req.method === 'GET') return next()
+  if (nonSecurePaths.includes(req.path)) return next()
+
+  //authenticate user
+  if(req.path.startsWith('/~')) {
+    // if some token in the request matches the given user...
+    return next()
+  }
+
+  res.send(401, 'Unauthorized');
+}
+*/
+
+app.get('/', (req, res) => {
+  res.send(`
+    <oauth-flow></oauth-flow>
+    <script type="module">
+      import tag from 'https://deno.land/x/tag@v0.3.2/mod.js'
+      const $ = tag('oauth-flow')
+
+      $.render(target => {
+        return 'hello world'
+      })
+    </script>
+  `)
+})
+
 app.use(bus.libs.http_in)
 
 app.listen(port, () => {
