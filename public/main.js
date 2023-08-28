f4c12ba05f5996b7cb05eec74f2c48fc7207ccf9
Tyler Childs <email@tychi.me>
2023-08-28 10:01:52 -0700

    swap what the word tag means and does and where

--- public/main.js
+++ public/main.js
@@ -13,33 +13,3 @@ import './packages/script-type.js'
 import './packages/field-text.js'
 import './packages/field-select.js'
 import './packages/connected-service.js'
-
-function tag(config) {
-  [...document.querySelectorAll(':not(:defined)')].forEach(async (target) => {
-    class WebComponent extends HTMLElement {
-      constructor() {
-        super();
-      }
-    }
-
-    const customElement = target.tagName.toLowerCase()
-    const url = `${config.proxy || '.'}/${customElement}.js`
-    const exists = (await fetch(url, {method: 'HEAD'})).ok
-    if(!exists) return
-    await import(url).catch(() => null)
-    if(target.matches(':not(:defined)')) {
-      customElements.define(
-        customElement,
-        WebComponent
-      );
-    }
-  })
-}
-
-(function(config) {
-  tag(config)
-  new MutationObserver((mutationsList) => {
-    console.log(mutationsList)
-    tag(config)
-  }).observe(document.body, { childList: true, subtree: true });
-})({ proxy: './tag' })
