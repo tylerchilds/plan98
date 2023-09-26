af208d9204fe1c143a7999f8e146033571e109b2
Tyler Childs <email@tychi.me>
2023-09-25 21:52:57 -0700

    deno edge

--- mod.js
+++ mod.js
@@ -1,13 +1,33 @@
-import express from 'npm:express'
-import request from "npm:request"
-import session from 'npm:express-session'
-import bodyParser from 'npm:body-parser'
+import { serve } from "https://deno.land/std@0.114.0/http/server.ts";
+import { Status } from "https://deno.land/std/http/http_status.ts";
+import * as path from "https://deno.land/std@0.184.0/path/mod.ts";
+import { typeByExtension } from "https://deno.land/std@0.186.0/media_types/type_by_extension.ts";
 
-const app = express()
-app.use(express.static('public'))
-app.use(bodyParser.urlencoded({extended: true}))
-app.use(bodyParser.json())
-app.use(bodyParser.raw())
+async function router(request, context) {
+  let { pathname } = new URL(request.url);
+  let extension = path.extname(pathname);
 
-// Create the HTTP server
-app.listen(3000, () => console.log('http://localhost:3000'))
+  let file
+  let statusCode = Status.Success
+  try {
+    file = await Deno.readFile(`./public${pathname}`)
+  } catch (e) {
+    pathname = './public/index.html'
+    extension = path.extname(pathname);
+    file = await Deno.readFile(pathname)
+    statusCode = Status.NotFound
+    console.error(pathname + '\n' + e)
+  }
+
+
+  return new Response(file, {
+    headers: {
+      'content-type': typeByExtension(extension),
+    },
+    status: statusCode
+  })
+}
+
+serve(router);
+console.log("Listening on http://localhost:8000");
+
