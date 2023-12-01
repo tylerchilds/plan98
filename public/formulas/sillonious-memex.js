7b295dea6587c7cc2473a91b42ed7cfcd80e14de
Tyler Childs <email@tychi.me>
2023-12-01 13:56:02 -0800

    make this smartwatch smart poppin'

--- public/formulas/sillonious-memex.js
+++ public/formulas/sillonious-memex.js
@@ -58,7 +58,7 @@ const tileMap = tiles.reduce((map, tile) => {
 }, {})
 
 $.draw((target) => {
-  const { diskette } = $.learn()
+  const { diskette, paused } = $.learn()
   const bin = diskettes(target)
   const game = bin[diskette]
 
@@ -67,20 +67,24 @@ $.draw((target) => {
     : `<iframe src="${protocol}${game}" title="${game}"></iframe>`
 
   target.innerHTML =`
-    <button name="${tileMap.shift.name}">
-      ${t(tileMap.shift.label)}
-    <button name="${tileMap.enter.name}">
-      ${t(tileMap.enter.label)}
-    </button>
-    <button name="${tileMap.tab.name}">
-      ${t(tileMap.tab.label)}
-    </button>
-    <button name="${tileMap.escape.name}">
-      ${t(tileMap.escape.label)}
-    </button>
-    </button>
-    <div name="carousel">
-      ${screen}
+    <div name="the-time-machine" class=${paused ? 'circus-enabled' : '' }>
+      <button name="${tileMap.shift.name}">
+        ${t(tileMap.shift.label)}
+      <button name="${tileMap.enter.name}">
+        ${t(tileMap.enter.label)}
+      </button>
+      <middle-earth></middle-earth>
+      <hr style="display: none;"/>
+      <div name="carousel">
+        ${screen}
+      </div>
+      <button name="${tileMap.tab.name}">
+        ${t(tileMap.tab.label)}
+      </button>
+      <button name="${tileMap.escape.name}">
+        ${t(tileMap.escape.label)}
+      </button>
+      </button>
     </div>
   `
 })
@@ -94,6 +98,8 @@ $.when('click', '[name="enter"]', () => {
 })
 
 $.when('click', '[name="tab"]', (event) => {
+  const { paused } = $.learn()
+  if(!paused) return
   let { diskette } = $.learn()
   const count = diskettes(event.target).length
   diskette = (diskette + 1) % count
@@ -101,15 +107,20 @@ $.when('click', '[name="tab"]', (event) => {
 })
 
 $.when('click', '[name="shift"]', () => {
-  alert('enhance')
+  const { paused } = $.learn()
+  $.teach({ paused: !paused })
 })
 
 $.style(`
-  & {
+  & [name="the-time-machine"] {
+    z-index: 1001;
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
     display: grid;
-    margin: 1rem 0;
-    background: black;
-    grid-template-rows: 1fr auto 1fr;
+    grid-template-rows: 2rem auto 2rem;
     grid-template-columns: 1fr 1fr;
     max-height: 100vh;
     overflow: hidden;
@@ -117,6 +128,34 @@ $.style(`
       "${tileMap.shift.name} ${tileMap.enter.name}"
       "${tileMap.carousel.name} ${tileMap.carousel.name}"
       "${tileMap.tab.name} ${tileMap.escape.name}";
+  }
+
+  & middle-earth {
+    grid-area: ${tileMap.carousel.name};
+    height: 100%;
+  }
+
+  & [name="carousel"] {
+    display: none;
+    place-self: center;
+  }
+
+  & .circus-enabled hr {
+    display: block !important;
+    position: absolute;
+    inset: 0;
+    margin: 0;
+    padding: 0;
+    border: 0;
+    height: 100%;
+    z-index: 1000;
+    background: rgba(0,0,0,.85);
+    grid-area: ${tileMap.carousel.name};
+  }
+
+  & .circus-enabled [name="carousel"] {
+    display: block;
+    z-index: 1001;
   }
 
   ${layout(tiles)}
