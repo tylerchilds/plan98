ae4ecb4cf0033a51b23012d1e1824a5ace8eaad7
Tyler Childs <email@tychi.me>
2023-12-11 18:11:55 -0800

    disable upsells

--- public/sillyz.computer.js
+++ public/sillyz.computer.js
@@ -5,7 +5,7 @@ export default class SillyzComputer {
       modules({ path })
     }).observe(document.body, { childList: true, subtree: true });
     ready()
-    upsell()
+    //upsell()
   }
 }
 
