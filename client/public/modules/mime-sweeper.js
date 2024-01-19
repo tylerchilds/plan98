a67adb95bc3011dd1a785822dcc7795aeaa54426
Tyler Childs <email@tychi.me>
2024-01-19 15:15:59 -0800

    mimesweeper

--- client/public/modules/mime-sweeper.js
+++ client/public/modules/mime-sweeper.js
@@ -1,55 +1,36 @@
 import module from '@sillonious/module'
+import { v4 as uuidv4 } from 'uuid';
 
 const $ = module('mime-sweeper', {
   keys: ['rows', 'columns', 'ratio', 'room'],
-  rows: 10,
-  columns: 10,
+  rows: 9,
+  columns: 12,
   ratio: .1,
   tick: 0,
   room: '0001',
   instances: {}
 })
 
-function infer(rows, columns, y, x, boxes) {
-  const minX = Math.max(0, x-1);
-  const maxX = Math.min(x+1, columns-1);
-  const minY = Math.max(0, y-1);
-  const maxY = Math.min(y+1, rows-1);
-
-  let count = 0
-  for(let a = minX; a <= maxX; a++) {
-    for(let b = minY; b <= maxY; b++) {
-      count += boxes[`${b}-${a}`].mimed ? 1 : 0
-    }
-  }
-
-  return count
-}
-
-
-setInterval(() => {
-  const tick = $.learn().tick+1
-  $.teach({ tick })
-}, 1000)
-
-function instance(target) {
-  const root = target.closest($.link)
-  return $.learn().instances[root.id]
-}
-
 $.draw((target) => {
   const { tick, instances } = $.learn()
   seed(target)
   if(!instances[target.id]) return
-  const { boxes, rows, columns } = instances[target.id]
+  const { finished, won, boxes, rows, columns } = instances[target.id]
 
   function createRow(row) {
     if(!boxes) return 'no boxes'
     return [...Array(columns).keys()].map(column => {
       const box = boxes[`${row}-${column}`] || {}
       return `
-        <button class="${ box.entered ? 'cell entered' : 'cell' }" data-row="${row}" data-column="${column}">
-          ${box.entered && box.count ? box.count : '' }
+        <button class="
+          cell
+          ${ box.revealed ? 'revealed' : '' }
+          ${ box.alive ? 'alive' : '' }
+          "
+          data-row="${row}"
+          data-column="${column}"
+        >
+          ${box.revealed ? (box.mimed ? 'x' : box.count ? box.count : ''):'' }
         </button>
       `
     }).join('')
@@ -59,7 +40,7 @@ $.draw((target) => {
 
   return `
     ${tick}
-    <div class="grid" style="--rows: ${rows}; --columns: ${columns};">
+    <div class="grid ${finished ? (won?'won':'lost') : ''}" style="--rows: ${rows}; --columns: ${columns};">
       ${grid}
     </div>
   `
@@ -68,12 +49,18 @@ $.draw((target) => {
 $.when('click', '.cell', (event) => {
   const { row, column } = event.target.dataset
   const { boxes, id, rows, columns } = instance(event.target)
-  const { mimed } = boxes[`${row}-${column}`]
+  const { mimed, count } = boxes[`${row}-${column}`]
   infer(rows, columns, parseInt(row), parseInt(column), boxes)
+
+  if(count === 0) {
+    pow(id, rows, columns, parseInt(row), parseInt(column), boxes)
+  }
+
   if(mimed) {
-    alert('game over')
+    updateBox({ id, x: column, y: row }, { revealed: true })
+    updateInstance({ id }, { finished: true, won: false })
   } else {
-    updateBox({ id, x: column, y: row }, { entered: true })
+    updateBox({ id, x: column, y: row }, { revealed: true })
   }
 })
 
@@ -83,10 +70,11 @@ function seed(target) {
   const { rows, columns, ratio, room } = $.learn() || {}
 
   const boxes = {}
+  let mimes = {}
   for(let y = 0; y < rows; y++) {
     for(let x = 0; x < columns; x++) {
       boxes[`${y}-${x}`] = {
-        entered: false,
+        revealed: false,
         mimed: false,
         alive: false,
         count: 0,
@@ -99,13 +87,13 @@ function seed(target) {
   function ensureRandomMime() {
     const y = Math.floor(Math.random() * rows)
     const x = Math.floor(Math.random() * columns)
-    if(boxes[`${y}-${x}`].mimed) {
+    const key = `${y}-${x}`
+    if(boxes[key].mimed) {
       ensureRandomMime()
     } else {
-      boxes[`${y}-${x}`].mimed = true
-        //updateBox({ id: target.id, x, y }, { mimed: true })
+      mimes[key] = uuidv4()
+      boxes[key].mimed = true
     }
-
   }
 
   function countMimeula() {
@@ -125,7 +113,7 @@ function seed(target) {
 
   schedule(() => {
     const id = target.id
-    updateInstance({ id }, { id, rows, columns, ratio, room, boxes })
+    updateInstance({ id }, { id, rows, columns, ratio, room, boxes, mimes })
   })
 }
 
@@ -166,7 +154,123 @@ function updateBox({ x, y, id }, payload) {
   })
 }
 
-function schedule(x) { setTimeout(x, 1) }
+function infer(rows, columns, y, x, boxes) {
+  const minX = Math.max(0, x-1);
+  const maxX = Math.min(x+1, columns-1);
+  const minY = Math.max(0, y-1);
+  const maxY = Math.min(y+1, rows-1);
+
+  let count = 0
+  for(let a = minX; a <= maxX; a++) {
+    for(let b = minY; b <= maxY; b++) {
+      count += boxes[`${b}-${a}`].mimed ? 1 : 0
+    }
+  }
+
+  return count
+}
+
+function pow(id, rows, columns, y, x, boxes) {
+  const minX = Math.max(0, x-1);
+  const maxX = Math.min(x+1, columns-1);
+  const minY = Math.max(0, y-1);
+  const maxY = Math.min(y+1, rows-1);
+
+  for(let a = minX; a <= maxX; a++) {
+    for(let b = minY; b <= maxY; b++) {
+      const { revealed } = boxes[`${b}-${a}`]
+      if(!revealed) {
+        updateBox({ id, x: a, y: b }, { revealed: true })
+      }
+    }
+  }
+}
+
+function reanimate(id, rows, columns, y, x, boxes) {
+  const minX = Math.max(0, x-1);
+  const maxX = Math.min(x+1, columns-1);
+  const minY = Math.max(0, y-1);
+  const maxY = Math.min(y+1, rows-1);
+
+  const soil = []
+  for(let a = minX; a <= maxX; a++) {
+    for(let b = minY; b <= maxY; b++) {
+      const { alive } = boxes[`${b}-${a}`]
+      if(!alive) {
+        soil.push([b,a])
+      }
+    }
+  }
+
+  if(soil.length === 0) return
+
+  const [b, a] = soil[Math.floor(Math.random() * soil.length)]
+  updateBox({ id, x: a, y: b }, { alive: true })
+}
+
+function life($, id) {
+  const { instances } = $.learn()
+  const { boxes, rows, columns } = instances[id]
+
+  const nextGenXboxes = Object.keys(boxes).reduce((all, box) => {
+    const { alive } = all[box]
+    let [y, x] = box.split('-')
+    y = parseInt(y)
+    x = parseInt(x)
+    const minX = Math.max(0, x-1);
+    const maxX = Math.min(x+1, columns-1);
+    const minY = Math.max(0, y-1);
+    const maxY = Math.min(y+1, rows-1);
+
+    let count = 0
+    for(let a = minX; a <= maxX; a++) {
+      for(let b = minY; b <= maxY; b++) {
+        const { alive } = boxes[`${b}-${a}`]
+        if(alive) {
+          count += 1
+        }
+      }
+    }
+
+    count = alive ? count : count - 1
+
+    console.log(count)
+    if((count >= 2 && count <= 3)) {
+      all[box].alive = true
+    } else if(alive) {
+      all[box].alive = false
+    }
+
+    return all
+  }, boxes)
+
+  updateInstance({ id }, { boxes: nextGenXboxes })
+}
+
+setInterval(() => {
+  const { tick, instances } = $.learn()
+
+  $.teach({ tick: tick+1 })
+
+  Object.keys(instances).map(id => {
+    const { mimes, boxes, rows, columns, finished } = instances[id]
+    if(finished) return
+    Object.keys(mimes).map(box => {
+      const [y, x] = box.split('-')
+      reanimate(id, rows, columns, y, x, boxes)
+    })
+
+    life($, id)
+  })
+
+}, 1000)
+
+function instance(target) {
+  const root = target.closest($.link)
+  return $.learn().instances[root.id]
+}
+
+function schedule(x, delay=1) { setTimeout(x, delay) }
 
 $.style(`
   & .grid {
@@ -176,13 +280,30 @@ $.style(`
     height: 100%;
   }
 
+  & .won {
+    opacity: .85;
+    pointer-events: none;
+  }
+
+  & .lost {
+    opacity: .5;
+    pointer-events: none;
+  }
+
   & .cell {
     border: .25rem solid var(--wheel-0-0);
     border-left-color: var(--wheel-0-1);
     border-top-color: var(--wheel-0-1);
+    background: var(--wheel-0-2);
+    color: var(--wheel-0-6);
+    filter: grayscale(1);
   }
 
-  & .entered {
+  & .revealed {
     border: none;
+  }
+
+  & .alive {
+    filter: grayscale(0);
   }
 `)
