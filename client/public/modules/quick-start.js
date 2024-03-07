70966a304ca965f87d12ef1831d35c1e0e2992ed
Tyler Childs <email@tychi.me>
2024-03-06 19:39:14 -0500

    Revert "young's computer"
    This reverts commit 9eb019645c36851ba6947f437b7612fe90065fe1.

--- client/public/modules/quick-start.js
+++ client/public/modules/quick-start.js
@@ -78,7 +78,7 @@ $.draw(() => {
         <div id="foreground">
           <div id="logo">
               <hypertext-variable id="vt1" monospace="0" slant="-15" casual="1" cursive="1" weight="800">
-                Young's
+                Sillyz.
               </hypertext-variable>
               <hypertext-variable id="vt2" monospace="1" slant="0" casual="0" cursive="0">
                 COMPUTER
