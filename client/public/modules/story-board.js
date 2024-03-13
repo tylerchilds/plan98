d8e4a8d1bfa1f8ef9f8bb14a42f957c8388efff5
Tyler Childs <email@tychi.me>
2024-03-13 14:44:33 -0400

    story-board basic implementation

---
+++ client/public/modules/story-board.js
@@ -0,0 +1,189 @@
+import module from '@sillonious/module'
+
+const $ = module('story-board')
+
+function engine(target) {
+  const canvas = target.closest($.link).querySelector('canvas')
+
+  return { canvas }
+}
+
+$.draw(target => {
+  return `
+    <canvas width="300px" height="300px">Sorry, your browser is too old for this demo.</canvas>
+    <div id="info">
+      <button data-undo>Undo</button>
+    </div>
+  `
+})
+
+let lineWidth = 0
+let isMousedown = false
+let points = []
+const strokeHistory = []
+
+const requestIdleCallback = window.requestIdleCallback || function (fn) { setTimeout(fn, 1) };
+
+/**
+ * This function takes in an array of points and draws them onto the canvas.
+ * @param {array} stroke array of points to draw on the canvas
+ * @return {void}
+ */
+function drawOnCanvas (target, stroke) {
+  const { canvas } = engine(target)
+  const context = canvas.getContext('2d')
+  context.strokeStyle = 'black'
+  context.lineCap = 'round'
+  context.lineJoin = 'round'
+
+  console.log(stroke)
+  const l = stroke.length - 1
+  if (stroke.length >= 3) {
+    const xc = (stroke[l].x + stroke[l - 1].x) / 2
+    const yc = (stroke[l].y + stroke[l - 1].y) / 2
+    context.lineWidth = stroke[l - 1].lineWidth
+    context.quadraticCurveTo(stroke[l - 1].x, stroke[l - 1].y, xc, yc)
+    context.stroke()
+    context.beginPath()
+    context.moveTo(xc, yc)
+  } else {
+    const point = stroke[l];
+    context.lineWidth = point.lineWidth
+    context.strokeStyle = point.color
+    context.beginPath()
+    context.moveTo(point.x, point.y)
+    context.stroke()
+  }
+}
+
+/**
+ * Remove the previous stroke from history and repaint the entire canvas based on history
+ * @return {void}
+ */
+$.when('click', '[data-undo]', function undoDraw (event) {
+  const { canvas } = engine(event.target)
+  const context = canvas.getContext('2d')
+  strokeHistory.pop()
+  context.clearRect(0, 0, canvas.width, canvas.height)
+
+  strokeHistory.map(function (stroke) {
+    if (strokeHistory.length === 0) return
+
+    context.beginPath()
+
+    let strokePath = [];
+    stroke.map(function (point) {
+      strokePath.push(point)
+      drawOnCanvas(event.target, strokePath)
+    })
+  })
+})
+
+$.when('touchstart', 'canvas', start)
+$.when('mousedown', 'canvas', start)
+
+function start(e) {
+  const { canvas } = engine(e.target)
+  const context = canvas.getContext('2d')
+  let pressure = 0.1;
+  let x, y;
+  if (e.touches && e.touches[0] && typeof e.touches[0]["force"] !== "undefined") {
+    if (e.touches[0]["force"] > 0) {
+      pressure = e.touches[0]["force"]
+    }
+    x = e.touches[0].clientX - canvas.offsetLeft
+    y = e.touches[0].clientY - canvas.offsetTop
+  } else {
+    pressure = 1.0
+    x = e.clientX - canvas.offsetLeft
+    y = e.clientY - canvas.offsetTop
+  }
+
+  isMousedown = true
+
+  lineWidth = Math.log(pressure + 1)
+  context.lineWidth = lineWidth// pressure * 50;
+
+  points.push({ x, y, lineWidth })
+  drawOnCanvas(e.target, points)
+}
+
+$.when('touchmove', 'canvas', move)
+$.when('mousemove', 'canvas', move)
+
+function move (e) {
+  const { canvas } = engine(e.target)
+  const context = canvas.getContext('2d')
+  if (!isMousedown) return
+  e.preventDefault()
+
+  let pressure = 0.1
+  let x, y
+  if (e.touches && e.touches[0] && typeof e.touches[0]["force"] !== "undefined") {
+    if (e.touches[0]["force"] > 0) {
+      pressure = e.touches[0]["force"]
+    }
+    x = e.touches[0].clientX - canvas.offsetLeft
+    y = e.touches[0].clientY - canvas.offsetTop
+  } else {
+    pressure = 1.0
+    x = e.clientX - canvas.offsetLeft
+    y = e.clientY - canvas.offsetTop
+  }
+
+  // smoothen line width
+  lineWidth = (Math.log(pressure + 1) * 4 * 0.2 + lineWidth * 0.8)
+  points.push({ x, y, lineWidth })
+
+  drawOnCanvas(e.target, points);
+
+  requestIdleCallback(() => {
+    $.teach({ pressure })
+
+    const touch = e.touches ? e.touches[0] : null
+    if (touch) {
+      $.teach({
+        touchesHTML: `
+          touchType = ${touch.touchType} ${touch.touchType === 'direct' ? '👆' : '✍️'} <br/>
+          radiusX = ${touch.radiusX} <br/>
+          radiusY = ${touch.radiusY} <br/>
+          rotationAngle = ${touch.rotationAngle} <br/>
+          altitudeAngle = ${touch.altitudeAngle} <br/>
+          azimuthAngle = ${touch.azimuthAngle} <br/>
+        `
+      })
+    }
+  })
+}
+
+$.when('touchend', 'canvas', end)
+$.when('touchleave', 'canvas', end)
+$.when('mouseup', 'canvas', end)
+function end (e) {
+  const { canvas } = engine(e.target)
+  const context = canvas.getContext('2d')
+  let pressure = 0.1;
+  let x, y;
+
+  if (e.touches && e.touches[0] && typeof e.touches[0]["force"] !== "undefined") {
+    if (e.touches[0]["force"] > 0) {
+      pressure = e.touches[0]["force"]
+    }
+    x = e.touches[0].clientX - canvas.offsetLeft
+    y = e.touches[0].clientY - canvas.offsetTop
+  } else {
+    pressure = 1.0
+    x = e.clientX - canvas.offsetLeft
+    y = e.clientY - canvas.offsetTop
+  }
+
+  isMousedown = false
+
+  requestIdleCallback(function () { strokeHistory.push([...points]); points = []})
+
+  lineWidth = 0
+};
+
+$.style(`
+  & canvas { background: lemonchiffon; }
+`)
