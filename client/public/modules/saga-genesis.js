3ba5cb518f6ed8c8ae54b3010b67cdf0d7258688
Tyler Childs <email@tychi.me>
2024-04-01 19:01:17 -0400

    make sure vision pro can have an escape key

--- client/public/modules/saga-genesis.js
+++ client/public/modules/saga-genesis.js
@@ -10,7 +10,7 @@ const emeraldOfTime = { saga: '/public/sagas/time.saga' }
 let quest = emeraldOfTime.saga
 
 function useMacGuffin(macGuffin) {
-  quest = macGuffin
+  $.teach({ quest: macGuffin })
   const {
     previousReality,
     reality
@@ -41,7 +41,16 @@ function getHost(target) {
 $.draw((target) => {
   const host = getHost(target)
   const chaosEmerald = doingBusinessAs[host] || emeraldOfTime
-  return useMacGuffin(chaosEmerald.saga)
+  const macGuffin = useMacGuffin(chaosEmerald.saga)
+
+  const escapeButton = `
+    <button data-escape>Esc</button>
+  `
+
+  return `
+    ${escapeButton}
+    ${macGuffin}
+  `
 })
 
 $.style(`
@@ -58,12 +67,47 @@ $.style(`
   }
 `)
 
+$.when('click', '[data-escape]', revealTruth)
+
 window.addEventListener('keydown', (event) => {
   if (event.key === 'Escape') {
-    showModal(`
-      <div style="overflow: auto; width: 100%; height: 100%; max-width: 100vw; max-height: 100vh; padding: 4rem .25rem;">
-        <code-module src="${quest}"></code-module>
-      </div>
-    `, { centered: true })
+    revealTruth()
   }
 });
+
+function revealTruth() {
+  showModal(`
+    <div style="overflow: auto; width: 100%; height: 100%; max-width: 100vw; max-height: 100vh; padding: 4rem .25rem;">
+      <code-module src="${$.learn().quest}"></code-module>
+    </div>
+  `, { centered: true })
+}
+
+$.style(`
+  & [data-escape] {
+    background: black;
+    border: none;
+    border-radius: 0 0 1rem 0;
+    color: white;
+    padding: 0 1rem 0 .5rem;
+    line-height: 1;
+    height: 2rem;
+    opacity: .8;
+    transition: opacity: 200ms;
+    position: absolute;
+    top: 0;
+    left: 0;
+    z-index: 1101;
+  }
+
+  & [data-escape]:hover,
+  & [data-escape]:focus {
+    cursor: pointer;
+    opacity: 1;
+  }
+
+  & [data-escape] * {
+    pointer-events: none;
+  }
+
+`)
