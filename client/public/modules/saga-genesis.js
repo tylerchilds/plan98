e42f616113c9acd65e70f37db6529aa111fc2de0
Tyler Childs <email@tychi.me>
2024-04-04 15:30:35 -0400

    hyperscript preview window

--- client/public/modules/saga-genesis.js
+++ client/public/modules/saga-genesis.js
@@ -75,6 +75,7 @@ function revealTruth(event) {
 
   showModal(`
     <div style="overflow: auto; width: 100%; height: 100%; max-width: 100vw; max-height: 100vh; padding: 2rem .25rem;">
+      <media-plexer src="${quest}"></media-plexer>
       <code-module src="${quest}" stack="${quest},${[...new Set(nautiloids)]}"></code-module>
     </div>
   `, { centered: true })
