52d36c268fa9d628d2d6dd2bf0bf252073f2d8b4
Tyler Childs <email@tychi.me>
2024-05-13 10:50:04 -0400

    bring in node deps

--- provision.sh
+++ provision.sh
@@ -6,6 +6,11 @@ curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 # install deno
 curl -fsSL https://deno.land/x/install/install.sh | sh
 
+# install nvm
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+source ~/.bashrc
+nvm install --lts
+
 # download 9p 
 wget https://github.com/pfpacket/rust-9p/archive/refs/heads/master.tar.gz -O - | tar -xz
 
