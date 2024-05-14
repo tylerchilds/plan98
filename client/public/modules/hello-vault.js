8ea71316dc9cf4b145332e950ba36a289b747a52
Tyler Childs <email@tychi.me>
2024-05-14 12:43:58 -0400

    basic locking and unlocking of the vault

--- client/public/modules/hello-vault.js
+++ client/public/modules/hello-vault.js
@@ -5,69 +5,62 @@ const appId = plan98.env.VAULT_APP_ID; // provided on admin panel
 const appSecret = plan98.env.VAULT_APP_SECRET; // provided on admin panel
 const appSalt = plan98.env.VAULT_APP_SALT; // provided on admin panel
 const localStorageMode = BayunCore.LocalDataEncryptionMode.EXPLICIT_LOGOUT_MODE;
-const enableFaceRecognition = true;
+const enableFaceRecognition = false;
 const baseURL = plan98.env.VAULT_BASE_URL; // provided on admin panel
-
-const NEVER_MODE = 'never'
-const INACTIVE_MODE = 'inactive'
-const ACTIVE_MODE = 'active'
 
 const bayunCore = BayunCore.init(appId, appSecret, appSalt,
   localStorageMode, enableFaceRecognition, baseURL);
 
-const $ = module('hello-vault', { mode: NEVER_MODE })
+const $ = module('hello-vault', {
+  sessionId: '',
+  companyName: 'sillyz.computer',
+  companyEmployeeId: '',
+  password: '',
+  autoCreateEmployee: true
+})
 
 $.draw((target) => {
-  const { mode } = $.learn()
+  const { sessionId, lockedText, unlockedText, error } = $.learn()
 
-  if(NEVER_MODE === mode) {
+  if(!sessionId) {
     return `
-      Hey, you've never been here... totally cool! In fact, splendid! Welcome.
-
-      <form action="registerWithPassword">
+      <div class="error">
+        ${error ? error : ''}
+      </div>
+      <form action="loginWithPassword">
         <label class="field">
-          <span class="label">Email</span>
-          <input class="hotlink" type="email" name="companyEmployeeId" required/>
+          <span class="label">Identity</span>
+          <input data-bind type="email" name="companyEmployeeId" required/>
         </label>
-
         <label class="field">
-          <span class="label">password</span>
-          <input class="hotlink" type="password" name="password" required/>
+          <span class="label">Password</span>
+          <input data-bind type="password" name="password" required/>
         </label>
-
-        <button type="submit">
-          Register
-        </button>
+        <rainbow-action>
+          <button type="submit">
+            Log In
+          </button>
+        </rainbow-action>
       </form>
     `
   }
 
-  if(INACTIVE_MODE === mode) {
-    return `
-      <button class="login">Login</button>
-    `
-  }
-
-  if(ACTIVE_MODE === mode) {
-    return `
-      <button class="logout">Logout</button>
-    `
-  }
+  return `
+    ${lockedText}: locked,
+    ${unlockedText}: unlocked
+  `
 })
 
-$.when('keyup', '.hotlink', event => {
+$.when('keyup', '[data-bind]', event => {
   const { name, value } = event.target;
   $.teach({ [name]: value })
 })
-
-const sessionId = "<sessionId>";
-const companyName = "sillyz.computer"; // company portion from loginId
 
 const securityQuestionsCallback = data => {
   if (data.sessionId) {
     if(data.authenticationResponse == BayunCore.AuthenticateResponse.VERIFY_SECURITY_QUESTIONS){
       let securityQuestionsArray = data.securityQuestions;
-      //securityQuestionsArray is a list of Security Question Objects with questionId, questionText
+      // securityQuestionsArray is a list of Security Question Objects with questionId, questionText 
       // Iterate through securityQuestionsArray
       securityQuestionsArray.forEach(val=>{
         console.log(val.questionId);
@@ -75,14 +68,14 @@ const securityQuestionsCallback = data => {
       });
       //Show custom UI to take user input for the answers.
       //Call validateSecurityQuestions function with the user provided answers.
-
+      
       //Here answers object is created just for reference
-      var answers=[];
-      answers.push({questionId: "<questionId1>", answer: "<answer1>"});
-      answers.push({questionId: "<questionId2>", answer: "<answer2>"});
-      answers.push({questionId: "<questionId3>", answer: "<answer3>"});
-      answers.push({questionId: "<questionId4>", answer: "<answer4>"});
-      answers.push({questionId: "<questionId5>", answer: "<answer5>"});
+       var answers=[]; 
+       answers.push({questionId: "<questionId1>", answer: "<answer1>"});
+       answers.push({questionId: "<questionId2>", answer: "<answer2>"});
+       answers.push({questionId: "<questionId3>", answer: "<answer3>"});
+       answers.push({questionId: "<questionId4>", answer: "<answer4>"});
+       answers.push({questionId: "<questionId5>", answer: "<answer5>"});
 
       const successCallback = data => {
         if (data.sessionId) {
@@ -90,10 +83,10 @@ const securityQuestionsCallback = data => {
         }};
 
       const failureCallback = error => {
-        console.error(error);
-      };
+          console.error(error);
+       };
 
-      bayunCore.validateSecurityQuestions(data.sessionId, answers, null, successCallback, failureCallback);
+       bayunCore.validateSecurityQuestions(data.sessionId, answers, null, successCallback, failureCallback);
     }
   }
 };
@@ -101,17 +94,16 @@ const securityQuestionsCallback = data => {
 const passphraseCallback = data => {
   if (data.sessionId) {
     if(data.authenticationResponse == BayunCore.AuthenticateResponse.VERIFY_PASSPHRASE){
-
-      //Show custom UI to take user input for the passphrase.
-      //Call validatePassphrase function with the user provided passphrase.
+     //Show custom UI to take user input for the passphrase.
+     //Call validatePassphrase function with the user provided passphrase.
 
       const successCallback = data => {
         if (data.sessionId) {
           //Passphrase validated and LoggedIn Successfully
-        }};
+      }};
 
       const failureCallback = error => {
-        console.error(error);
+          console.error(error);
       };
 
       bayunCore.validatePassphrase(data.sessionId, "<passphrase>", null, successCallback, failureCallback);
@@ -119,48 +111,43 @@ const passphraseCallback = data => {
   }
 };
 
-const successCallback = data => {
-  if (data.sessionId) {
-    //LoggedIn Successfully
-  }
+const successCallback = async data => {
+  const {
+    sessionId,
+    authenticationResponse,
+    employeeAlreadyExists,
+    securityQuestions
+  } = data
+  var lockedText = await bayunCore.lockText(sessionId, "Hello, Future World.");
+
+  var unlockedText = await bayunCore.unlockText(sessionId, lockedText);
+  $.teach({ sessionId, authenticationResponse, employeeAlreadyExists, securityQuestions, lockedText, unlockedText })
 };
 
 const failureCallback = error => {
-  console.error(error);
+  $.teach({ error: `${error}` })
 };
 
-const authorizeEmployeeCallback = (data) => {
-  if (data.sessionId) {
-    if (data.authenticationResponse == BayunCore.AuthenticateResponse.AUTHORIZATION_PENDING) {
-      // You can get employeePublicKey in data.employeePublicKey for it's authorization
-    }
-  }
-};
+$.when('click', '[type="submit"]', (event) => {
+  $.teach({ error: null })
 
-$.when('submit', '[action="registerWithPassword"]', (event) => {
-  event.preventDefault()
-  const { companyEmployeeId, password } = $.learn()
-  alert(`${companyEmployeeId}, ${password}`)
-  bayunCore.registerEmployeeWithPassword(
+  const {
     sessionId,
     companyName,
     companyEmployeeId,
     password,
-    authorizeEmployeeCallback,
-    successCallback,
-    failureCallback
-  );
-})
+    autoCreateEmployee
+  } = $.learn()
 
-$.when('click', '.login', () => {
-  const { companyEmployeeId } = $.learn()
   bayunCore.loginWithPassword(
-    sessionId,
-    companyName,
-    companyEmployeeId,
-    securityQuestionsCallback,
-    passphraseCallback,
-    successCallback,
-    failureCallback
+      sessionId,
+      companyName,
+      companyEmployeeId,
+      password,
+      autoCreateEmployee,
+      securityQuestionsCallback,
+      passphraseCallback,
+      successCallback,
+      failureCallback
   );
 })
