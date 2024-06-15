50641ea13ee0a2904e1c0a25a744785fc06d15c8
Tyler Childs <email@tychi.me>
2024-06-15 19:13:52 -0400

    begin forking the comedy notebook to be a boring business to business notebook

--- client/public/modules/bayun-login.js
+++ client/public/modules/bayun-login.js
@@ -1,166 +1,545 @@
-import module from '@silly/tag'
-import { BayunCore } from '@sillonious/vault'
+import module, { state } from '@silly/tag'
+import { bayunCore } from '@sillonious/vault'
+import supabase from '@sillonious/database'
+import { render } from '@sillonious/saga'
+import { showModal } from '@plan98/modal'
 
-const appId = plan98.env.VAULT_APP_ID; // provided on admin panel
-const appSecret = plan98.env.VAULT_APP_SECRET; // provided on admin panel
-const appSalt = plan98.env.VAULT_APP_SALT; // provided on admin panel
-const localStorageMode = BayunCore.LocalDataEncryptionMode.EXPLICIT_LOGOUT_MODE;
-const enableFaceRecognition = true;
-const baseURL = plan98.env.VAULT_BASE_URL; // provided on admin panel
-
-const NEVER_MODE = 'never'
-const INACTIVE_MODE = 'inactive'
-const ACTIVE_MODE = 'active'
-
-const bayunCore = BayunCore.init(appId, appSecret, appSalt,
-  localStorageMode, enableFaceRecognition, baseURL);
-
-const $ = module('bayun-login', { mode: NEVER_MODE })
+const $ = module('bayun-login', {
+  sessionId: '',
+  companyName: plan98.host,
+  companyEmployeeId: '',
+  password: '',
+  autoCreateEmployee: true,
+  jokes: {}
+})
 
 $.draw((target) => {
-  const { mode } = $.learn()
+  const { error, jokes, active } = $.learn()
+  const { sessionId } = getSession()
 
-  if(NEVER_MODE === mode) {
+  target.style.backgroundImage = getStars(target)
+
+  if(!sessionId) {
     return `
-      Hey, you've never been here... totally cool! In fact, splendid! Welcome.
+      <div name="login">
+        Login
+        <div class="error">
+          ${error ? error : ''}
+        </div>
+        <form method="POST" action="loginWithPassword">
+          <label class="field">
+            <span class="label">Console</span>
+            <input data-bind type="text" name="companyName" value="${target.getAttribute('companyName')}" required/>
+          </label>
+          <label class="field">
+            <span class="label">Player</span>
+            <input data-bind type="text" name="companyEmployeeId" value="${target.getAttribute('companyEmployeeId')}" required/>
+          </label>
+          <label class="field">
+            <span class="label">Password</span>
+            <input data-bind type="password" name="password" required/>
+          </label>
+          <rainbow-action>
+            <button type="submit">
+              Log In
+            </button>
+          </rainbow-action>
+        </form>
+      </div>
+    `
+  }
 
-      <form action="registerWithPassword">
-        <label class="field">
-          <span class="label">Email</span>
-          <input class="hotlink" type="email" name="companyEmployeeId" required/>
-        </label>
+  const lines = getLines(target)
 
-        <label class="field">
-          <span class="label">password</span>
-          <input class="hotlink" type="password" name="password" required/>
-        </label>
-
-        <button type="submit">
-          Register
+  const focus = document.activeElement
+  target.innerHTML = `
+    <div class="page" >
+      <div class="actions">
+        <button data-new>
+          New Joke
         </button>
-      </form>
-    `
-  }
+        <button data-clear>
+          Clear Jokes
+        </button>
+        <button data-logout>
+          Logout
+        </button>
 
-  if(INACTIVE_MODE === mode) {
-    return `
-      <button class="login">Login</button>
-    `
-  }
+      </div>
+      <div class="setlist">
+        ${Object.keys(jokes).map((id) => {
+          return `
+            <div name="joke" class="index-card">
+              <input name="setup" type="text" data-id="${id}" value="${jokes[id].setup}" />
+              <textarea name="punchline" data-id="${id}" style="background-image: ${lines}">${jokes[id].punchline}</textarea>
+              <div class="joke-actions">
+                <button data-preview data-id="${id}">
+                  Preview
+                </button>
+                <button data-save data-id="${id}">
+                  Save
+                </button>
+              </div>
+            </div>
+          `
+        }).join('')}
+      </div>
+    </div>
+  `
+})
 
-  if(ACTIVE_MODE === mode) {
-    return `
-      <button class="logout">Logout</button>
-    `
+$.when('click', '[data-save]', async (event) => {
+  const { id } = event.target.dataset
+  const setup = event.target.closest($.link).querySelector(`[data-id="${id}"][name="setup"]`).value
+  let punchline = event.target.closest($.link).querySelector(`[data-id="${id}"][name="punchline"]`).value
+  const {
+    sessionId,
+    companyName,
+    companyEmployeeId
+  } = getSession()
+
+  punchline = await bayunCore.lockText(sessionId, punchline);
+
+  const { data, error } = await supabase
+  .from('plan98_solo_text')
+  .update(
+    { setup, punchline }
+  )
+  .eq('id', id)
+
+  if(error) {
+    $.teach({ error })
+    return
   }
 })
 
-$.when('keyup', '.hotlink', event => {
+$.when('click', '[data-back]', () => {
+  $.teach({ activeSetup: null })
+})
+
+$.when('click', '[data-logout]', async () => {
+  clearSession()
+  $.teach({ jokes: {} })
+})
+
+$.when('click', '[data-new]', async () => {
+  const {
+    sessionId,
+    companyName,
+    companyEmployeeId
+  } = getSession()
+ 
+  const setup = '';
+  const punchline = await bayunCore.lockText(sessionId, '');
+  const { data, error } = await supabase
+  .from('plan98_solo_text')
+  .insert([
+    { companyName, companyEmployeeId, setup, punchline },
+  ])
+  .select()
+
+  if(error) {
+    $.teach({ error })
+    return
+  }
+})
+
+$.when('click', '[data-preview]', async (event) => {
+  const { id } = event.target.dataset
+  const punchline = event.target.closest($.link).querySelector(`[data-id="${id}"][name="punchline"]`).value
+
+  showModal(`
+    <div style="background: rgba(200,200,200,.65); width: 100%; height: 100%;">
+    <div style="overflow: auto; background: rgba(255,255,255,.85); margin: 0 auto; width: 8.5in; padding: 1in 1in 1in 1.5in; height: 100%; font-size: 1.5rem; line-height: 2rem;">
+      ${render(punchline)}
+    </div>
+  `)
+})
+
+
+$.when('click', '[data-clear]', async () => {
+  const {
+    companyName,
+    companyEmployeeId
+  } = getSession()
+
+  const { error } = await supabase
+  .from('plan98_solo_text')
+  .delete()
+  .eq('companyName', companyName)
+  .eq('companyEmployeeId', companyEmployeeId)
+
+  if(error) {
+    $.teach({ error })
+    return
+  }
+})
+
+$.when('input', '[data-bind]', event => {
   const { name, value } = event.target;
   $.teach({ [name]: value })
 })
 
-const sessionId = "<sessionId>";
-const companyName = "sillyz.computer"; // company portion from loginId
+export function getSession() {
+  return state['ls/bayun'] || {}
+}
 
-const securityQuestionsCallback = data => {
-  if (data.sessionId) {
-    if(data.authenticationResponse == BayunCore.AuthenticateResponse.VERIFY_SECURITY_QUESTIONS){
-      let securityQuestionsArray = data.securityQuestions;
-      //securityQuestionsArray is a list of Security Question Objects with questionId, questionText
-      // Iterate through securityQuestionsArray
-      securityQuestionsArray.forEach(val=>{
-        console.log(val.questionId);
-        console.log(val.questionText);
-      });
-      //Show custom UI to take user input for the answers.
-      //Call validateSecurityQuestions function with the user provided answers.
+export function clearSession() {
+  state['ls/bayun'] = {}
+}
 
-      //Here answers object is created just for reference
-      var answers=[];
-      answers.push({questionId: "<questionId1>", answer: "<answer1>"});
-      answers.push({questionId: "<questionId2>", answer: "<answer2>"});
-      answers.push({questionId: "<questionId3>", answer: "<answer3>"});
-      answers.push({questionId: "<questionId4>", answer: "<answer4>"});
-      answers.push({questionId: "<questionId5>", answer: "<answer5>"});
+function setSession({ sessionId, companyName, companyEmployeeId }) {
+  state['ls/bayun'] = {
+    sessionId,
+    companyName,
+    companyEmployeeId
+  }
+}
 
-      const successCallback = data => {
-        if (data.sessionId) {
-          //Security Questions' Answers validated and LoggedIn Successfully
-        }};
+async function laugh(premise) {
+  const {
+    sessionId,
+  } = getSession()
 
-      const failureCallback = error => {
-        console.error(error);
-      };
+  return await bayunCore.unlockText(sessionId, premise);
+}
 
-      bayunCore.validateSecurityQuestions(data.sessionId, answers, null, successCallback, failureCallback);
+const successCallback = async data => {
+  const {
+    companyName,
+    companyEmployeeId,
+  } = $.learn()
+
+  const {
+    sessionId,
+    authenticationResponse,
+    employeeAlreadyExists,
+    securityQuestions
+  } = data
+  var lockedText = await bayunCore.lockText(sessionId, "Hello, Future World.");
+
+  var unlockedText = await bayunCore.unlockText(sessionId, lockedText);
+  $.teach({ authenticationResponse, employeeAlreadyExists, securityQuestions, lockedText, unlockedText })
+
+  setSession({ sessionId, companyName, companyEmployeeId })
+
+  connect()
+};
+
+connect()
+
+function comedyPath() {
+  const {
+    companyName,
+    companyEmployeeId
+  } = getSession()
+
+  return ['', companyName, companyEmployeeId, 'Comedy'].join('/')
+}
+
+async function connect() {
+  const {
+    sessionId,
+    companyName,
+    companyEmployeeId
+  } = getSession()
+
+  if(!sessionId) return
+  
+  let { data: plan98_solo_text, error } = await supabase
+  .from('plan98_solo_text')
+  .select("*")
+  // Filters
+  .eq('companyName', companyName)
+  .eq('companyEmployeeId', companyEmployeeId)
+  .range(0, 25)
+
+  plan98_solo_text.map(async (row) => {
+    const setup = row.setup
+    const punchline = await bayunCore.unlockText(sessionId, row.punchline)
+
+    $.teach({ id: row.id, setup, punchline }, mergeJoke)
+  })
+
+  supabase.channel('custom-all-channel')
+  .on(
+    'postgres_changes',
+    { event: '*', schema: 'public', table: 'plan98_solo_text' },
+    async (payload) => {
+      if (
+        payload.new.companyName === companyName &&
+        payload.new.companyEmployeeId === companyEmployeeId
+      ) {
+        const setup = payload.new.setup
+        const punchline = await bayunCore.unlockText(sessionId, payload.new.punchline)
+
+        $.teach({ id: payload.new.id, setup, punchline }, mergeJoke)
+      }
+
+      if(payload.eventType === 'DELETE') {
+        $.teach({ id: payload.old.id }, deleteJoke)
+      }
+    }
+  )
+  .subscribe()
+}
+
+function mergeJoke(state, payload) {
+  return {
+    ...state,
+    jokes: {
+      ...state.jokes,
+      [payload.id]: { 
+        punchline: payload.punchline,
+        setup: payload.setup
+      }
     }
   }
-};
+}
 
-const passphraseCallback = data => {
-  if (data.sessionId) {
-    if(data.authenticationResponse == BayunCore.AuthenticateResponse.VERIFY_PASSPHRASE){
-
-      //Show custom UI to take user input for the passphrase.
-      //Call validatePassphrase function with the user provided passphrase.
-
-      const successCallback = data => {
-        if (data.sessionId) {
-          //Passphrase validated and LoggedIn Successfully
-        }};
-
-      const failureCallback = error => {
-        console.error(error);
-      };
-
-      bayunCore.validatePassphrase(data.sessionId, "<passphrase>", null, successCallback, failureCallback);
-    }
+function deleteJoke(state, payload) {
+  const newState = {
+    ...state,
   }
-};
 
-const successCallback = data => {
-  if (data.sessionId) {
-    //LoggedIn Successfully
-  }
-};
+  delete newState.jokes[payload.id]
+
+  return newState
+}
+
 
 const failureCallback = error => {
-  console.error(error);
+  $.teach({ error: `${error}` })
 };
 
-const authorizeEmployeeCallback = (data) => {
-  if (data.sessionId) {
-    if (data.authenticationResponse == BayunCore.AuthenticateResponse.AUTHORIZATION_PENDING) {
-      // You can get employeePublicKey in data.employeePublicKey for it's authorization
-    }
-  }
-};
-
-$.when('submit', '[action="registerWithPassword"]', (event) => {
+$.when('submit', 'form', (event) => {
   event.preventDefault()
-  const { companyEmployeeId, password } = $.learn()
-  alert(`${companyEmployeeId}, ${password}`)
-  bayunCore.registerEmployeeWithPassword(
-    sessionId,
+})
+$.when('click', '[type="submit"]', (event) => {
+  $.teach({ error: null })
+
+  const {
     companyName,
     companyEmployeeId,
     password,
-    authorizeEmployeeCallback,
-    successCallback,
-    failureCallback
-  );
-})
+  } = $.learn()
 
-$.when('click', '.login', () => {
-  const { companyEmployeeId } = $.learn()
-  bayunCore.loginWithPassword(
-    sessionId,
+
+  __prototypeAuth(
     companyName,
     companyEmployeeId,
-    securityQuestionsCallback,
-    passphraseCallback,
+    password
+  )
+})
+
+export function __prototypeAuth(companyName, companyEmployeeId, password) {
+  bayunCore.loginWithPassword(
+    null, // sessionId
+    companyName,
+    companyEmployeeId,
+    password,
+    true, // autoCreateEmployee
+    null, // TODO: @bayun, what is?
+    noop.bind('securityQuestionsCallback'),
+    noop.bind('passphraseCallback'),
     successCallback,
     failureCallback
   );
-})
+}
+
+function noop(){}
+
+function getLines(target) {
+  const canvas = document.createElement("canvas");
+  const ctx = canvas.getContext('2d');
+
+  const rhythm = parseFloat(getComputedStyle(target).getPropertyValue('line-height'));
+  canvas.height = rhythm;
+  canvas.width = rhythm;
+
+  ctx.fillStyle = 'transparent';
+  ctx.fillRect(0, 0, rhythm, rhythm);
+
+  ctx.fillStyle = 'dodgerblue';
+  ctx.fillRect(0, rhythm - (rhythm), rhythm, 1);
+
+  return `url(${canvas.toDataURL()}`;
+}
+
+function getStars(target) {
+  const color = 'rgba(0,0,0,.85)';
+  const canvas = document.createElement("canvas");
+  const ctx = canvas.getContext('2d');
+
+  const rhythm = parseFloat(getComputedStyle(document.documentElement).fontSize);
+
+  canvas.height = rhythm;
+  canvas.width = rhythm;
+
+  ctx.fillStyle = color;
+  ctx.fillRect(rhythm / 2, rhythm / 2, 1, 1);
+
+  return `url(${canvas.toDataURL()})`;
+}
+
+$.style(`
+  & {
+    overflow: auto;
+    min-height: 480px;
+    display: block;
+    height: 100%;
+    width: 100%;
+    line-height: 2rem;
+    position: relative;
+    padding: 6rem 0;
+  }
+
+  & *:focus {
+    border-color: orange;
+    outline-color: orange
+  }
+
+  & .actions {
+    margin: 0 1rem;
+    position: fixed;
+    top: 4rem;
+    right: 0;
+    text-align: right;
+    z-index: 10;
+  }
+
+  & .joke-actions {
+    display: flex;
+    gap: 1rem;
+    padding: 0 1rem;
+    position: absolute;
+    bottom: 0;
+    z-index: 100;
+    right: 0;
+  }
+  & .actions button,
+  & .joke-actions button {
+    background: lemonchiffon;
+    color: saddlebrown;
+    border: none;
+    line-height: 1rem;
+    box-shadow: 0px 0px 4px 4px rgba(0,0,0,.10);
+    padding: .5rem;
+    font-size: 1rem;
+    --v-font-mono: 0;
+    --v-font-casl: 1;
+    --v-font-wght: 800;
+    --v-font-slnt: -15;
+    --v-font-crsv: 1;
+    font-variation-settings: "MONO" var(--v-font-mono), "CASL" var(--v-font-casl), "wght" var(--v-font-wght), "slnt" var(--v-font-slnt), "CRSV" var(--v-font-crsv);
+    font-family: "Recursive";
+    transition: background 200ms ease-in-out;
+  }
+
+  & .actions button:focus,
+  & .joke-actions button:focus,
+  & .actions button:hover,
+  & .joke-actions button:hover {
+    background: saddlebrown;
+    color: lemonchiffon;
+  }
+
+  & .page {
+    height: 100%;
+    padding: 0 0 5rem;
+  }
+
+  & .actions {
+    grid-area: actions;
+  }
+
+  & .setlist {
+    grid-area: list;
+    display: grid;
+    grid-template-columns: repeat(auto-fill, minmax(5in, 1fr));
+    gap: 2rem;
+  }
+
+  & .index-card {
+    width: 5in;
+    height: 3in;
+    position: relative;
+    margin: 0 auto;
+    z-index: 2;
+    display: grid;
+    grid-template-rows: auto 1fr;
+    box-shadow:
+      0px 0px 4px 4px rgba(0,0,0,.10),
+      0px 0px 12px 12px rgba(0,0,0,.05);
+    max-width: 100%;
+  }
+
+  & [name="setup"] {
+    font-size: 2rem;
+    border: none;
+    border-bottom: 3px solid orange;
+    padding: .5rem 1rem;
+    width: 100%;
+  }
+
+  & [name="punchline"] {
+    width: 100%;
+    height: 100%;
+    resize: none;
+    border: none;
+    padding: 0rem 1rem;
+    line-height: 2rem;
+    background-color: white;
+    position: relative;
+    z-index: 3;
+    background-position-y: -1px;
+  }
+
+  & .joke {
+    display: grid;
+    grid-area: active;
+    background: rgba(200,200,200,1);
+  }
+
+  & .paper {
+    margin: 0 auto;
+    max-width: 55ch;
+    background: rgba(255,255,255,1);
+    height: 100%;
+    overflow: auto;
+    resize: none;
+    width: 100%;
+    border: none;
+    margin: auto;
+  }
+
+  & [name="login"] {
+    max-width: 320px;
+    background: linear-gradient(rgba(255,255,255,.85), lemonchiffon);
+    margin: auto;
+    inset: 0;
+    padding: 1rem;
+    position: absolute;
+    box-shadow:
+      0px 0px 4px 4px rgba(0,0,0,.10),
+      0px 0px 12px 12px rgba(0,0,0,.05);
+  }
+
+  & .label {
+    color: saddlebrown;
+  }
+  & .title {
+    margin-bottom: 1rem;
+    display: block;
+    color: saddlebrown;
+  }
+
+  & [type="submit"] {
+    width: 100%;
+  }
+`)
+
+$.when('scroll', 'textarea', function({ target }) {
+    const scrollTop = target.scrollTop;
+    target.style.backgroundPosition = `0px ${-scrollTop}px`;
+});
--- client/public/modules/poker-face.js
+++ client/public/modules/poker-face.js
@@ -7,22 +7,26 @@ $.draw(() => {
   const { sessionId, companyName, companyEmployeeId } = getSession()
 
   return sessionId ? `
-    <div class="player">
-      ${companyEmployeeId}
-    </div>
     <div class="console">
-      ${companyName}
+      <div class="data">
+        ${companyName}
+      </div>
+    </div>
+    <div class="player">
+      <div class="data">
+        ${companyEmployeeId}
+      </div>
     </div>
     <button data-disconnect>
       Disconnect
     </button>
   ` : `
     <form>
-      <div class="player">
-        <input placeholder="player" name="companyEmployeeId" />
-      </div>
       <div class="console">
         <input type="text" placeholder="console" name="companyName" />
+      </div>
+      <div class="player">
+        <input placeholder="player" name="companyEmployeeId" />
       </div>
       <button type="submit">
         Connect
@@ -37,7 +41,11 @@ $.when('submit', 'form', async (event) => {
 
   const companyEmployeeId = event.target.companyEmployeeId.value
   const companyName = event.target.companyName.value
-  debugger
+
+  showModal(`
+    <bayun-login companyEmployeeId="${companyEmployeeId}" companyName="${companyName}">
+    </bayun-login>
+  `)
 })
 
 $.when('click', '[data-disconnect]', async () => {
@@ -52,8 +60,8 @@ $.style(`
   }
 
   & button {
-    background-image: linear-gradient(rgba(255,255,255,.5), rba(255,255,255,.25));
-    background-color: dodgerblue;
+    background-image: linear-gradient(rgba(0,0,0,.5), rgba(0,0,0,.75));
+    background-color: lime;
     color: white;
     display: block;
     border: 0;
@@ -67,25 +75,34 @@ $.style(`
     flex: none;
   }
 
+  & [data-disconnect] {
+    background-color: orange;
+  }
+
+
   & button:hover,
   & button:focus {
-    background: dodgerblue;
+    background-color: rebeccapurple;
     color: white;
   }
 
   & .player {
-    padding: 4rem 1rem 1rem;
   }
 
   & .console {
-    text-align: right;
     background: rgba(128,128,128,.5);
-    padding: 1rem;
+    padding-top: 3rem;
   }
 
   & input {
     border: none;
     background: transparent;
     color: rgba(255,255,255,.85);
+    padding: 1rem;
+    max-width: 100%;
+  }
+
+  & .data {
+    padding: 1rem;
   }
 `)
