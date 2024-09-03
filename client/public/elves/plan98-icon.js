6c2f60b3909296b3d34b010d5dec2eeafea973e0
Tyler Childs <email@tychi.me>
2024-09-03 16:47:12 -0400

    plan98 color theory

--- client/public/elves/plan98-icon.js
+++ client/public/elves/plan98-icon.js
@@ -1,64 +1,10 @@
-const symbols = `
-    <svg>
-      <defs>
+import "@shoelace-style/shoelace/icon"
+import elf from '@silly/tag'
 
-</defs>
-    </svg>
-  `;
-
-/* initialize */
-const icons = new DOMParser()
-  .parseFromString(symbols, "text/xml")
-  .querySelectorAll('g[id]');
-const ids = [...icons].map(node => node.id);
-const selectors = ids.map(id => `icon-${id}`);
-
-const $ = module('plan98-icon')
-
-initializeIconTags();
-initializeIcons();
+const $ = elf('plan98-icon')
 
 $.draw((target) => {
-  const { icon } = target.dataset
   return `
-    <svg viewBox="0 0 16 16" class="${$.link}">
-      <use xlink:href="#${icon}"></use>
-    </svg>
+    <sl-icon name="${target.getAttribute('name')}"></sl-icon>
   `
 })
-
-function initializeIconTags() {
-  ids.map(bind);
-
-  function bind(id) {
-    const selector = `icon-${id}`;
-    on('render', selector, function renderWrapper(event) {
-      render(event, id)
-    });
-  }
-
-  function render(event, id) {
-    event.target.innerHTML = `
-    `;
-  }
-}
-
-function initializeIcons() {
-  const iconHTML = `
-    <style type="text/css">
-      .${$.link} {
-        width: 1em;
-        height: 1em;
-      }
-
-      ${selectors.join(',')} {
-        pointer-events: none;
-      }
-    </style>
-    ${symbols}
-  `;
-
-  document
-    .body
-      .insertAdjacentHTML("beforeend", iconHTML);
-}
--- client/public/elves/plan98-intro.js
+++ client/public/elves/plan98-intro.js
@@ -1,13 +1,31 @@
 import elf from '@silly/tag'
 
 const palette = [
-  'firebrick',
-  'darkorange',
-  'gold',
-  'mediumseagreen',
-  'dodgerblue',
-  'mediumpurple'
+  'firebrick', // accent 1
+  'darkorange', // accent 2
+  'gold', // accent 3
+  'mediumseagreen', // underline
+  'dodgerblue', // action
+  'mediumpurple' // background
 ]
+
+const buttons = {
+  firebrick: ['mediumpurple'],
+  darkorange: ['firebrick'],
+  gold: ['darkorange'],
+  mediumseagreen: ['gold'],
+  dodgerblue: ['mediumseagreen'],
+  mediumpurple: ['dodgerblue'],
+}
+
+const underlines = {
+  firebrick: ['dodgerblue'],
+  darkorange: ['mediumpurple'],
+  gold: ['firebrick'],
+  mediumseagreen: ['darkorange'],
+  dodgerblue: ['gold'],
+  mediumpurple: ['mediumseagreen'],
+}
 
 const friends = {
   firebrick: ['darkorange', 'gold', 'mediumseagreen'],
@@ -18,31 +36,37 @@ const friends = {
   mediumpurple: ['firebrick', 'darkorange', 'gold'],
 }
 
-const $ = elf('plan98-intro')
+const $ = elf('plan98-intro', {
+  accents: ['firebrick', 'darkorange', 'gold']
+})
 
 $.draw((target) => {
-  const { menu, started } = $.learn()
+  const { menu, started, accents } = $.learn()
   const src = target.getAttribute('src')
   return `
     <div class="wall ${started && !menu ? 'broken':''}">
       <div>
       <div class="frame">
-        <div class="logo-mark">
-          <div class="plan98-letters">
-            Plan98
-          </div>
-          <div class="plan98-slants">
-            <div class="slant-1"></div>
-            <div class="slant-2"></div>
-            <div class="slant-3"></div>
+        <div style="text-align: center">
+          <div class="logo-mark">
+            <div class="plan98-letters">
+              Plan98
+            </div>
+            <div class="plan98-slants">
+              <div class="slant-1" style="--color: ${accents[0]}"></div>
+              <div class="slant-2" style="--color: ${accents[1]}"></div>
+              <div class="slant-3" style="--color: ${accents[2]}"></div>
+            </div>
           </div>
         </div>
-        <div class="about">
-          Plan98 is an operating system of the historical fiction variety. It did not release in the year 1998. Or did it? If it did, connect. If not, you will be unable to take part in the spoils after ending the time loop of 2038.
+        <div  ${started ? 'style="visibility: hidden;"' : ''}>
+          <div class="about">
+            Plan98 is an operating system of the historical fiction variety. It did not release in the year 1998. Or did it? If it did, connect. If not, you will be unable to take part in the spoils after ending the time loop of 2038.
+          </div>
+          <button class="break-fourth-wall">
+            Connect
+          </button>
         </div>
-        <button class="break-fourth-wall" ${started ? 'style="visibility: hidden;"' : ''}>
-          Connect
-        </button>
       </div>
       </div>
     </div>
@@ -65,14 +89,19 @@ $.draw((target) => {
 })
 
 const settingsInterval = setInterval(() => {
-  const color = palette[Math.floor(Math.random() * palette.length)];
+  const index = Math.floor(Math.random() * palette.length)
+  const color = palette[index];
   [...document.querySelectorAll($.link)].map(target => {
     target.style.setProperty('--color', color);
-    [...target.querySelectorAll('[class^="slant-"]')].map((node, i) => {
-      node.style.setProperty('--color', friends[color][i]);
+    target.style.setProperty('--button-color', buttons[color]);
+    target.style.setProperty('--underline-color', underlines[color]);
+    const accents = [...target.querySelectorAll('[class^="slant-"]')].map((node, i) => {
+      const currentC = friends[color][i]
+      node.style.setProperty('--color', currentC);
+      return currentC
     })
   })
-}, 5000)
+}, 2500)
 
 $.when('click', '[data-toggle]', async (event) => {
   const { menu } = $.learn()
@@ -87,6 +116,40 @@ $.when('click', '.break-fourth-wall', (event) => {
   $.teach({ started: true })
   clearInterval(settingsInterval)
 })
+
+let once = false
+export function superKey() {
+  if(once) return
+  once = true
+
+  function handleEvent (event) {
+    if (event.metaKey) {
+      handleMetaKey()
+    }
+  }
+  self.addEventListener('keydown', handleEvent);
+
+  self.addEventListener('message', function handleMessage(event) {
+  if(event.data.whisper === 'metaKey') {
+    handleMetaKey()
+  } else { console.log(event) }
+
+  });
+}
+
+function handleMetaKey() {
+  if(document.querySelector('plan98-intro')) {
+    const { menu } = $.learn()
+    $.teach({ menu: !menu })
+    return
+  }
+
+  if(self.self !== self.top) {
+    self.parent.postMessage({ whisper: 'metaKey' }, "*");
+  } else {
+    document.body.insertAdjacentHTML("beforeend", '<plan98-intro></plan98-intro>')
+  }
+}
 
 $.style(`
   & {
@@ -130,6 +193,8 @@ $.style(`
     height: 50px;
     display: grid;
     place-content: center;
+    font-weight: 800;
+    font-size: 24px;
   }
 
   & .menu-items {
@@ -168,7 +233,8 @@ $.style(`
     font-family: 'Recursive';
     font-size: 72px;
     position: relative;
-    margin: 2rem;
+    margin: 1rem 0;
+    display: inline-block;
   }
 
   & .frame {
@@ -209,10 +275,12 @@ $.style(`
     z-index: 2;
     color: rgba(255,255,255,1);
     text-shadow: 1px 1px rgba(0,0,0,1);
+    border-bottom: 1rem solid var(--underline-color, mediumseagreen);
+    padding: 0 1rem;
   }
 
   & .wall {
-    background-color: var(--color);
+    background-color: var(--color, mediumpurple);
     background-image: linear-gradient(-25deg, rgba(0,0,0,.85), rgba(0,0,0,.5));
     display: grid;
     place-content: center;
@@ -225,7 +293,7 @@ $.style(`
   }
 
   & .wall button {
-    background-color: var(--color);
+    background-color: var(--button-color, dodgerblue);
     background-image: linear-gradient(rgba(0,0,0, .25), rgba(0,0,0,.5));
     color: white;
     border: none;
