05fa82b6525187d7e0e8d797281c668a9649ad0e
Tyler Childs <email@tychi.me>
2024-09-23 16:09:30 -0400

    add loading indicators for action-script

--- client/public/elves/action-script.js
+++ client/public/elves/action-script.js
@@ -24,22 +24,31 @@ $.draw(target => {
 export async function actionScript(event) {
   const root = event.target.closest($.link) || event.target.closest('.'+$.link)
   const { action, script, saga } = root.dataset
+  root.classList.add('active')
+  const otherActions = [...document.querySelectorAll(`${$.link}:not(.active)`)]
+  otherActions.map((x) => {
+    x.classList.add('inactive')
+  })
   if(script) {
-    const dispatch = (await import(script))[action]
+        const dispatch = (await import(script))[action]
     if(dispatch) {
       self.history.pushState({ action, script }, "");
       await dispatch(event, root)
     }
-
   }
   if(saga) {
     paste(saga, event.target, {})
   }
+
+  root.classList.remove('active')
+  otherActions.map((x) => {
+    x.classList.remove('inactive')
+  })
 }
 
 $.when('click', 'button', actionScript)
 
-function paste(nextSaga, target, options={}) {
+async function paste(nextSaga, target, options={}) {
   // first issue: []
   const root = target.closest('.wizard-journey') || target.closest('wizard-journey') || target.closest('main') || target.closest('body')
   const host = root.getAttribute('host')
@@ -49,7 +58,7 @@ function paste(nextSaga, target, options={}) {
 
   target.dataset.lastHtml = target.innerHTML
   target.innerHTML = `<a href="${key}">Loading...</a>`
-  fetch(raw+key)
+  await fetch(raw+key)
     .then(async response => {
       if(response.status === 404) {
         target.innerHTML = target.dataset.lastHtml
@@ -119,6 +128,29 @@ $.style(`
     padding: 0 1rem;
   }
 
+  &.inactive {
+    opacity: .5;
+    pointer-events: none;
+    text-indent: -200%;
+  }
+
+  &.active button {
+    pointer-events: none;
+    text-indent: -200%;
+    overflow: hidden;
+  }
+
+  &.active button::before {
+    content: '';
+    position: absolute;
+    inset: 0;
+    height: 75%;
+    aspect-ratio: 1;
+    margin: auto;
+    animation: loader 3000ms ease-in-out alternate infinite;
+    background: lemonchiffon;
+  }
+
   & button {
     background: linear-gradient(rgba(0,0,0,.25), rgba(0,0,0,.5));
     background-color: dodgerblue;
@@ -129,6 +161,7 @@ $.style(`
     transition: background-color 200ms ease-in-out;
     padding: 1rem;
     width: 100%;
+    position: relative;
   }
 
   & button:focus,
