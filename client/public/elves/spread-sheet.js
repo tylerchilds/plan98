ca2f4ecf1c8bdad94e51df4ee0097ef797046e2f
Tyler Childs <email@tychi.me>
2024-11-30 22:58:41 -0800

    make dad's spreadsheets more like spreadsheets

--- client/public/elves/spread-sheet.js
+++ client/public/elves/spread-sheet.js
@@ -1,6 +1,5 @@
 import elf from '@silly/elf'
-import { Grid } from 'ag-grid-community';
-import Papa from 'papaparse'
+import jspreadsheet from 'jspreadsheet-ce'
 
 const $ = elf('spread-sheet', { rowData: [] })
 
@@ -9,42 +8,18 @@ function mods(x) {
   }
   return table[x] ? table[x] : {}
 }
-
 $.draw((target) => {
   const { rowData } = $.learn()
 
   if(!target.fetched) {
     target.fetched = true
-    Papa.parse(target.getAttribute('src'), {
-      download: true,
-      complete: function(results) {
-        if(results.errors.length === 0) {
-          $.teach({ rowData: results.data })
-        }
-      }
-    })
+
+    jspreadsheet(target, {
+      csv: target.getAttribute('src'),
+      defaultColWidth: 120,
+      csvHeaders:true,
+    });
   }
-
-  target.classList.add('ag-theme-custom')
-
-  const sample = rowData[0]
-
-  if(!sample) return
-
-  const columnDefs = sample.map((x, i) => {
-    return { headerName: x, field: `${i}`, flex: 1, ...mods(x) }
-  })
-
-  const gridOptions = {
-    rowHeight: '42px',
-    columnDefs,
-    rowData: rowData.slice(1, -1),
-    autoSizeStrategy: {
-      type: 'fitCellContents'
-    },
-  };
-
-  new Grid(target, gridOptions);
 })
 
 $.style(`
@@ -58,12 +33,11 @@ $.style(`
   }
 `)
 
+
 // Dynamically load AG Grid CSS
-const cssUrl1 = 'https://esm.sh/ag-grid-community@31.3.1/styles/ag-grid.css';
-const cssUrl2 = '/cdn/fantasysports.social/table.css';
+const cssUrl1 = 'https://esm.sh/jspreadsheet-ce@4.15.0/dist/jspreadsheet.css';
 
 loadCSS(cssUrl1);
-loadCSS(cssUrl2);
 
 function loadCSS(url) {
   const link = document.createElement('link');
