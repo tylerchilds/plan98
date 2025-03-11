bdadd483dce7b3131fec7728a7036a8958d70f80
Tyler Childs <email@tychi.me>
2025-03-11 14:25:37 -0700

    flying disk

---
+++ client/public/elves/flying-disk.js
@@ -0,0 +1,65 @@
+import elf from '@silly/elf'
+
+const $ = elf('flying-disk')
+
+$.draw(() => {
+  return `
+    <div class="track">
+      <div class="disk"></div>
+    </div>
+  `
+})
+
+$.style(`
+  & {
+    display: block;
+    overflow: hidden;
+    animation: &-fading-background ease-in-out 2000ms infinite alternate;
+  }
+
+  & .track {
+    animation: &-sliding-track ease-in-out 2000ms infinite alternate;
+    width: 100%;
+    height: 2rem;
+    display: grid;
+    align-items: center;
+  }
+
+  & .disk {
+    animation: &-flying-disk ease-in-out 2000ms infinite alternate;
+    width: 1rem;
+    height: 1rem;
+    background: lemonchiffon;
+  }
+
+  @keyframes &-fading-background {
+    0% {
+      background: rgba(0,0,0,.85);
+    }
+    100% {
+      background: rgba(0,0,0,.5);
+    }
+  }
+  @keyframes &-sliding-track {
+    0% {
+      transform: translateX(0)
+    }
+    100% {
+      transform: translateX(100%);
+    }
+  }
+
+
+  @keyframes &-flying-disk {
+    0% {
+      transform: rotateX(0deg) rotateZ(0deg);
+    }
+    50% {
+      transform: rotateX(30deg) rotateZ(1080deg);
+    }
+
+    100% {
+      transform: rotateX(80deg) rotateZ(1540deg);
+    }
+  }
+`)
