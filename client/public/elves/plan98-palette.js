474c029d5fcc962abb199b9a3c19698b64dc3012
Tyler Childs <email@tychi.me>
2025-05-29 09:48:58 -0700

    initial aural checkpoint for the palette

--- client/public/elves/plan98-palette.js
+++ client/public/elves/plan98-palette.js
@@ -1,8 +1,37 @@
+import Wad from 'web-audio-daw';
 import elf from '@plan98/elf'
+
+const midiRange = [...new Array(128)].map((x, i) => i)
+
+const defaultConfig = {
+  source : 'sawtooth',
+  tuna   : {
+    Overdrive : {
+      outputGain: 0.5,         //0 to 1+
+      drive: 0.7,              //0 to 1
+      curveAmount: 1,          //0 to 1
+      algorithmIndex: 0,       //0 to 5, selects one of our drive algorithms
+      bypass: 0
+    },
+    Chorus : {
+      intensity: 0.3,  //0 to 1
+      rate: 4,         //0.001 to 8
+      stereoPhase: 0,  //0 to 180
+      bypass: 0
+    }
+  }
+}
+
+export function newSynth(config = defaultConfig) {
+  return new Wad(config)
+}
 
 const $ = elf('plan98-palette')
 
-$.draw(() => {
+$.draw((target) => {
+  if(!target.synth) {
+    target.synth = newSynth()
+  }
   return `
     <div class="colors">
       <div class="color" style="--color: var(--red, firebrick)"></div>
@@ -12,24 +41,58 @@ $.draw(() => {
       <div class="color" style="--color: var(--blue, dodgerblue)"></div>
       <div class="color" style="--color: var(--indigo, slateblue)"></div>
       <div class="color" style="--color: var(--violet, mediumpurple)"></div>
+      <div class="color" style="--color: var(--gray, dimgray)"></div>
     </div>
     <div class="toppings">
-      <div class="color" style="--color: rgba(255, 255, 255,1)"></div>
-      <div class="color" style="--color: rgba(255, 255, 255,.85)"></div>
-      <div class="color" style="--color: rgba(255, 255, 255,.65)"></div>
-      <div class="color" style="--color: rgba(255, 255, 255,.5)"></div>
-      <div class="color" style="--color: rgba(255, 255, 255,.35)"></div>
-      <div class="color" style="--color: rgba(255, 255, 255,.2)"></div>
-      <div class="color" style="--color: transparent"></div>
-      <div class="color" style="--color: rgba(0, 0, 0,.2)"></div>
-      <div class="color" style="--color: rgba(0, 0, 0,.35)"></div>
-      <div class="color" style="--color: rgba(0, 0, 0,.5)"></div>
-      <div class="color" style="--color: rgba(0, 0, 0,.65)"></div>
-      <div class="color" style="--color: rgba(0, 0, 0,.85)"></div>
       <div class="color" style="--color: rgba(0, 0, 0,1)"></div>
+      <div class="color" style="--color: rgba(0, 0, 0,.85)"></div>
+      <div class="color" style="--color: rgba(0, 0, 0,.65)"></div>
+      <div class="color" style="--color: rgba(0, 0, 0,.5)"></div>
+      <div class="color" style="--color: rgba(0, 0, 0,.35)"></div>
+      <div class="color" style="--color: rgba(0, 0, 0,.2)"></div>
+      <div class="color" style="--color: transparent"></div>
+      <div class="color" style="--color: rgba(255, 255, 255,.2)"></div>
+      <div class="color" style="--color: rgba(255, 255, 255,.35)"></div>
+      <div class="color" style="--color: rgba(255, 255, 255,.5)"></div>
+      <div class="color" style="--color: rgba(255, 255, 255,.65)"></div>
+      <div class="color" style="--color: rgba(255, 255, 255,.85)"></div>
+      <div class="color" style="--color: rgba(255, 255, 255,1)"></div>
+    </div>
+    <div class="tabs">
+      ${midiRange.map(renderNote).join('')}
     </div>
   `
 })
+
+function renderNote(midi) {
+  return `
+    <button aria-label="${midi}" data-play="${midi}">midi</button>
+  `
+}
+
+function attack(event) {
+	event.preventDefault()
+  const { play } = event.target.dataset
+  const { synth } = event.target.closest($.link)
+  synth.stop(play);
+  synth.play({ pitch: play, label: play });
+}
+
+function release (event) {
+	event.preventDefault()
+  const { play } = event.target.dataset
+  const { synth } = event.target.closest($.link)
+  synth.stop(play);
+}
+
+$.when('mouseenter', '[data-play]', attack)
+$.when('mouseleave', '[data-play]', release)
+
+$.when('mousedown', '[data-play]', attack)
+$.when('mouseup', '[data-play]', release)
+
+$.when('touchstart', '[data-play]', attack)
+$.when('touchend', '[data-play]', release)
 
 $.style(`
   & {
