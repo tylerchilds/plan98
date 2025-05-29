ab06a4625654744b99753f46160c5b77875b8eeb
Tyler Childs <email@tychi.me>
2025-05-29 11:31:05 -0700

    add sound effects to the palette i guess

--- client/public/elves/plan98-palette.js
+++ client/public/elves/plan98-palette.js
@@ -1,37 +1,18 @@
-import Wad from 'web-audio-daw';
 import elf from '@plan98/elf'
+import {
+  attack,
+  release,
+  attackRelease,
+  getNoteDuration
+} from './paper-pocket.js'
 
 const midiRange = [...new Array(128)].map((x, i) => i)
 
-const defaultConfig = {
-  source : 'sawtooth',
-  tuna   : {
-    Overdrive : {
-      outputGain: 0.5,         //0 to 1+
-      drive: 0.7,              //0 to 1
-      curveAmount: 1,          //0 to 1
-      algorithmIndex: 0,       //0 to 5, selects one of our drive algorithms
-      bypass: 0
-    },
-    Chorus : {
-      intensity: 0.3,  //0 to 1
-      rate: 4,         //0.001 to 8
-      stereoPhase: 0,  //0 to 180
-      bypass: 0
-    }
-  }
-}
-
-export function newSynth(config = defaultConfig) {
-  return new Wad(config)
-}
+const synths = {}
 
 const $ = elf('plan98-palette')
 
 $.draw((target) => {
-  if(!target.synth) {
-    target.synth = newSynth()
-  }
   return `
     <div class="colors">
       <div class="color" style="--color: var(--red, firebrick)"></div>
@@ -45,7 +26,9 @@ $.draw((target) => {
     </div>
     <div class="toppings">
       <div class="color" style="--color: rgba(0, 0, 0,1)"></div>
-      <div class="color" style="--color: rgba(0, 0, 0,.85)"></div>
+      <div class="color" style="--color: rgba(0, 0, 0,.95)"></div>
+      <div class="color" style="--color: rgba(0, 0, 0,.875)"></div>
+      <div class="color" style="--color: rgba(0, 0, 0,.775)"></div>
       <div class="color" style="--color: rgba(0, 0, 0,.65)"></div>
       <div class="color" style="--color: rgba(0, 0, 0,.5)"></div>
       <div class="color" style="--color: rgba(0, 0, 0,.35)"></div>
@@ -56,6 +39,7 @@ $.draw((target) => {
       <div class="color" style="--color: rgba(255, 255, 255,.5)"></div>
       <div class="color" style="--color: rgba(255, 255, 255,.65)"></div>
       <div class="color" style="--color: rgba(255, 255, 255,.85)"></div>
+      <div class="color" style="--color: rgba(255, 255, 255,.95)"></div>
       <div class="color" style="--color: rgba(255, 255, 255,1)"></div>
     </div>
     <div class="tabs">
@@ -66,33 +50,30 @@ $.draw((target) => {
 
 function renderNote(midi) {
   return `
-    <button aria-label="${midi}" data-play="${midi}">midi</button>
+    <button aria-label="${midi}" data-midi="${midi}">${midi}</button>
   `
 }
 
-function attack(event) {
-	event.preventDefault()
-  const { play } = event.target.dataset
-  const { synth } = event.target.closest($.link)
-  synth.stop(play);
-  synth.play({ pitch: play, label: play });
+function queueAttack(event) {
+  event.preventDefault()
+  const { midi } = event.target.dataset
+  attack(midi)
 }
 
-function release (event) {
-	event.preventDefault()
-  const { play } = event.target.dataset
-  const { synth } = event.target.closest($.link)
-  synth.stop(play);
+function queueRelease (event) {
+  event.preventDefault()
+  const { midi } = event.target.dataset
+  release(midi)
 }
 
-$.when('mouseenter', '[data-play]', attack)
-$.when('mouseleave', '[data-play]', release)
+$.when('mouseenter', '[data-midi]', queueAttack)
+$.when('mouseleave', '[data-midi]', queueRelease)
 
-$.when('mousedown', '[data-play]', attack)
-$.when('mouseup', '[data-play]', release)
+$.when('mousedown', '[data-midi]', queueAttack)
+$.when('mouseup', '[data-midi]', queueRelease)
 
-$.when('touchstart', '[data-play]', attack)
-$.when('touchend', '[data-play]', release)
+$.when('touchstart', '[data-midi]', queueAttack)
+$.when('touchend', '[data-midi]', queueRelease)
 
 $.style(`
   & {
@@ -120,5 +101,34 @@ $.style(`
     background-color: var(--color);
     width: 100%;
     height: 100%;
+  }
+
+  & .tabs {
+    display: grid;
+    grid-area: spot;
+    grid-template-columns: repeat(16, 1fr);
+    grid-template-rows: repeat(8, 1fr);
+    grid-auto-flow: column;
+  }
+
+  & .tabs [data-midi] {
+    border: 0;
+    background: transparent;
+    display: grid;
+    place-content: end;
+    color: white;
+    text-shadow:
+      0 0 3px rgba(0,0,0,.15),
+      0 0 2px rgba(0,0,0,.25),
+      1px 1px rgba(0,0,0,.45);
+
+
+    font-size: 12px;
+    opacity: 0;
+  }
+
+  & .tabs [data-midi]:hover,
+  & .tabs [data-midi]:focus {
+    opacity: 1;
   }
 `)
