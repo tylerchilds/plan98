6a9430e9f1a70d6b5a69057d0ed10c5d6b0e3520
Tyler Childs <email@tychi.me>
2025-06-12 12:32:05 -0700

    add was to boot priority in plan98

---
+++ server/was/provision.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Download the repository as a .tar.gz file
+curl -L -o wallet-attached-storage-server.tar.gz https://github.com/did-coop/wallet-attached-storage-server/archive/main.tar.gz
+
+# Extract the downloaded archive
+tar -xzvf wallet-attached-storage-server.tar.gz
+
+# Navigate into the extracted directory
+cd wallet-attached-storage-server-main
+
+# Run npm install
+npm install
+
+# Run deno task start-server
+
+# CORS_ALLOWED_ORIGINS='["http://localhost:8081"]' npm run dev
