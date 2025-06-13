77c484892395ede8b3a51beee85e550c3b357a7f
Tyler Childs <email@tychi.me>
2025-06-13 00:12:24 -0700

    @gobengo ty -ty

--- client/public/elves/was-hello.js
+++ client/public/elves/was-hello.js
@@ -1,6 +1,6 @@
 import { StorageClient } from "@wallet.storage/fetch-client";
 import { Ed25519Signer } from "@did.coop/did-key-ed25519"
-import elf from '@silly/elf'
+import elf from '@plan98/elf'
 import { toast } from './plan98-toast.js'
 
 const $ = elf('was-hello', {
@@ -11,13 +11,13 @@ const $ = elf('was-hello', {
 
 let signer
 async function init(target) {
-  if(target.initialized) return
+  if (target.initialized) return
   target.initialized = true
 
   const { host } = $.learn()
   const credentials = localStorage.getItem('was/signer')
 
-  if(credentials) {
+  if (credentials) {
     signer = await Ed25519Signer.fromJSON(credentials)
   } else {
     // This signer can create cryptographic signatures
@@ -37,7 +37,7 @@ async function init(target) {
   const resource = space.resource(target.getAttribute('src') || '/tmp')
   const response = await resource.get()
     .then(res => {
-      if(res.status === 200) {
+      if (res.status === 200) {
         const indexUrl = new URL(resource.path, storageUrl)
         $.teach({ home: indexUrl.toString() })
       }
@@ -63,13 +63,15 @@ async function publish(spaceId, src) {
     id: `urn:uuid:${spaceId}`
   })
 
+  const linkset = space.resource(`linkset`)
   const spaceObject = {
     controller: signer.controller,
-
+    // configure which resource to use as a linkset
+    link: linkset.path,
   }
   const spaceObjectBlob = new Blob(
     [JSON.stringify(spaceObject)],
-    {type:'application/json'},
+    { type: 'application/json' },
   )
 
   // send PUT request to update the space
@@ -84,8 +86,15 @@ async function publish(spaceId, src) {
       toast(e.message, { type: 'error' })
     })
 
-  if(!responseToPutSpace) return
+  if (!responseToPutSpace.ok) throw new Error(
+    `Failed to put space: ${responseToPutSpace.status} ${responseToPutSpace.statusText}`, {
+    cause: {
+      responseToPutSpace
+    }
+  })
+  if (!responseToPutSpace) return
 
+  // GET the space to make sure the PUT persisted it
   const responseToGetSpace = await space.get()
     .then(res => {
       console.debug({ res })
@@ -97,11 +106,17 @@ async function publish(spaceId, src) {
       toast(e.message, { type: 'error' })
     })
 
-  if(!responseToGetSpace) return
+  if (!responseToGetSpace.ok) throw new Error(
+    `Failed to get space: ${responseToGetSpace.status} ${responseToGetSpace.statusText}`, {
+    cause: {
+      responseToGetSpace
+    }
+  })
+  if (!responseToGetSpace) return
 
-  const resource = space.resource(src)
+  const index = space.resource(src)
   const blobForIndex = new Blob(['<!doctype html><h1>Hello WAS!</h1>'], { type: 'text/html' })
-  const responseToPutIndex = await resource.put(blobForIndex, { signer })
+  const responseToPutIndex = await index.put(blobForIndex, { signer })
     .then(res => {
       console.debug({ res })
       toast(JSON.stringify(res), { type: 'success' })
@@ -112,9 +127,60 @@ async function publish(spaceId, src) {
       toast(e.message, { type: 'error' })
     })
 
-  if(!responseToPutIndex) return
+  if (!responseToPutIndex.ok) throw new Error(`Failed to put index: ${responseToPutIndex.status} ${responseToPutIndex.statusText}`, {
+    cause: {
+      responseToPutIndex
+    }
+  })
 
-  const indexUrl = new URL(resource.path, storageUrl)
+  if (!responseToPutIndex) return
+
+  // Add a policy that makes things PublicCanRead
+  const aclAllowingPublicReads = space.resource('policy/published')
+  {
+    const policy = { type: 'PublicCanRead' }
+    const policyBlob = new Blob([JSON.stringify(policy)], { type: 'application/json' })
+    const responseToPutPolicy = await aclAllowingPublicReads.put(policyBlob, { signer })
+      .then(res => {
+        if (!res.ok) throw new Error(`Failed to put policy: ${res.status} ${res.statusText}`, { cause: { res } })
+        toast(`Created ${aclAllowingPublicReads.path}`, { type: 'success' })
+        return res
+      })
+      .catch(e => {
+        console.error(e)
+        toast(e.message, { type: 'error' })
+      })
+  }
+
+  // Update linkset so the index acl is aclAllowingPublicReads
+  {
+    const linksetObject = {
+      "linkset": [
+        {
+          "anchor": index.path,
+          "acl": [
+            {
+              "href": aclAllowingPublicReads.path,
+            }
+          ]
+        },
+        {
+          "anchor": space.resource(`tmp`).path,
+          "acl": [
+            {
+              "href": aclAllowingPublicReads.path,
+            }
+          ]
+        }
+      ]
+    };
+    const linksetBlob = new Blob([JSON.stringify(linksetObject)], { type: 'application/linkset+json' })
+    const response = await linkset.put(linksetBlob)
+    if (!response.ok) throw new Error(`Failed to put linkset: ${response.status} ${response.statusText}`, { cause: { response } });
+    toast(`PUT linkset with acl`, { type: 'success' })
+  }
+
+  const indexUrl = new URL(index.path, storageUrl)
   $.teach({ home: indexUrl.toString() })
 }
 
@@ -122,7 +188,7 @@ $.draw((target) => {
   init(target)
   const { host, home, loading } = $.learn()
 
-  if(loading) {
+  if (loading) {
     return `
       <div style="width: 100%; height: 100%;">
         <flying-disk></flying-disk>
@@ -146,13 +212,13 @@ $.draw((target) => {
 })
 
 $.when('submit', '[action="test-publish"]', async (event) => {
-  const root = event.target.closest($.link)
   event.preventDefault()
+  const root = event.target.closest($.link)
   publish(root.id, root.getAttribute('src') || '/tmp')
 })
 
 $.when('input', '[data-bind]', (event) => {
-  $.teach({[event.target.name]: event.target.value })
+  $.teach({ [event.target.name]: event.target.value })
 })
 
 $.style(`
@@ -176,7 +242,7 @@ $.style(`
 `)
 
 function escapeHyperText(text = '') {
-  return text.replace(/[&<>'"]/g, 
+  return text.replace(/[&<>'"]/g,
     actor => ({
       '&': '&amp;',
       '<': '&lt;',
