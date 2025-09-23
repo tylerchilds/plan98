712aeb917b5ed877c481ea8f014e833c5cebe07f
Tyler Childs <email@tychi.me>
2025-09-23 07:41:44 -0700

    clean up main street for the circus

--- client/public/main.js
+++ client/public/main.js
@@ -4,17 +4,13 @@ import('./elves/plan98-console.js').catch(console.error)
 
 self.plan98 ||= { env: {} }
 
-self.requestIdleCallback = self.requestIdleCallback || function (fn) { setTimeout(fn, 1) };
-
 const parameters = new URLSearchParams(window.location.search)
 const world = parameters.get('world')
-const database = localStorage.getItem("plan98.database") || self.plan98.env.POCKETBASE_URL;
 
 self.plan98 = {
   ...self.plan98,
   parameters,
-  database,
-  host: world ? window.location.host : 'sillyz.computer',
+  host: world ? world : window.location.host,
   registry: {}
 }
 
@@ -22,6 +18,7 @@ const style = document.createElement('link')
 
 style.setAttribute('href', `/public/cdn/${window.plan98.host}/default.css`)
 style.setAttribute('rel', `stylesheet`)
+
 document.head.appendChild(style)
 
 const newpage = `
@@ -31,11 +28,6 @@ const newpage = `
 import('@silly/elf').then(x => {
   x.default('#main').draw(target => newpage)
 })
-
-
-export function setDatabase(url) {
-  localStorage.setItem("plan98.database", 'https://sillonious.pockethost.io')
-}
 
 if(parameters.get('debug') === 'true') {
   document.body.insertAdjacentHTML('beforeend', `
