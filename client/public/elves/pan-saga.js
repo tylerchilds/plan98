df805c0134cd6d2c0b61654503919faa1158321a
Tyler Childs <email@tychi.me>
2026-03-01 09:39:08 -0800

    import pan-saga and lit deps and vendorize

--- client/public/elves/pan-saga.js
+++ client/public/elves/pan-saga.js
@@ -1,3 +1,10 @@
-import 'https://non-activity.stream/elements/pan-saga.js?importmap&define'
+import { PanSaga } from "pan-saga";
+const tag = "pan-saga"
+customElements.define(tag, PanSaga);
 
+/*
+  and this is where you add custom mods when the standards orgs get the api wrong
+*/
 
+// break in case of emergency
+// import('@plan98/types').then(({Self})=> { const $ = Self(tag)/*; Self($)*/ })
